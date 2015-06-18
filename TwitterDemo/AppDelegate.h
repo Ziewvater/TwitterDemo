2a7466dbf9a40f967764f9f2e4f4cf7c548e959a
Jeremy Lawrence <Jeremy.Y.Lawrence@gmail.com>
2015-06-18 09:58:19 -0700

    Initial Commit

---
+++ TwitterDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterDemo
+//
+//  Created by Jeremy Lawrence on 6/18/15.
+//  Copyright (c) 2015 Ziewvater. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
